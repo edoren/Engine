a9e1b99e047e324b5fec722862a455e78e52d661
Manuel Sabogal <mfer32@gmail.com>
2017-02-18 00:38:10 -0500

    Added Shader and Texture2D abstract classes.

--- src/Renderer/Shader.hpp
+++ src/Renderer/Shader.hpp
@@ -1,3 +1,23 @@
 #pragma once
 
-#include <Renderer/OpenGL/GL_Shader.hpp> //TMP
+#include <Util/Prerequisites.hpp>
+
+#include <Math/Math.hpp>
+#include <System/String.hpp>
+
+namespace engine {
+
+class ENGINE_API Shader : NonCopyable {
+public:
+    Shader() {}
+    virtual ~Shader() {}
+
+    virtual Shader& operator=(Shader&& other) = default;
+
+    virtual bool LoadFromMemory(const String& vertex_source,
+                                const String& fragment_source) = 0;
+
+    virtual void Use() = 0;
+};
+
+}  // namespace engine
--- src/Renderer/Texture2D.hpp
+++ src/Renderer/Texture2D.hpp
@@ -1,3 +1,20 @@
 #pragma once
 
-#include <Renderer/OpenGL/GL_Texture2D.hpp> //TMP
+#include <Graphics/Image.hpp>
+
+namespace engine {
+
+class ENGINE_API Texture2D : NonCopyable {
+public:
+    Texture2D() {}
+
+    virtual ~Texture2D() {}
+
+    virtual Texture2D& operator=(Texture2D&& other) = default;
+
+    virtual bool LoadFromImage(const Image& img) = 0;
+
+    virtual void Use() = 0;
+};
+
+}  // namespace engine
