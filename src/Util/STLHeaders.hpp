5a816a75ba1628db50a0aa00c546a6517c4f07b4
Manuel Sabogal <mfer32@gmail.com>
2017-02-18 08:14:01 -0500

    Added header file to encapsulate all the STL headers.

---
+++ src/Util/STLHeaders.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <bitset>
+#include <deque>
+#include <functional>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+#include <algorithm>
+#include <chrono>
+#include <numeric>
+#include <random>
+#include <type_traits>
+
+#if _WIN32
+#include <filesystem>
+#else
+#include <experimental/filesystem>
+#endif
