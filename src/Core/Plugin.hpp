0687e934e82f1055e09ce2fbdf2bf95ae6c701e1
Manuel Sabogal <mfer32@gmail.com>
2017-02-17 23:57:17 -0500

    Added Plugin abstract class.

---
+++ src/Core/Plugin.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <System/String.hpp>
+
+namespace engine {
+
+class Plugin {
+public:
+    virtual ~Plugin() {}
+
+    virtual const String& GetName() const = 0;
+
+    virtual void Install() = 0;
+
+    virtual void Initialize() = 0;
+
+    virtual void Shutdown() = 0;
+
+    virtual void Uninstall() = 0;
+};
+
+}  // namespace engine
