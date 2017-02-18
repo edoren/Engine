6058db7a4c096ced58f92c078d75ed4fcc1bcea5
Manuel Sabogal <mfer32@gmail.com>
2017-02-18 00:18:05 -0500

    Added types to handle UTF-8, UTF-16 and UTF-32 characters.

--- src/Util/Types.hpp
+++ src/Util/Types.hpp
@@ -4,16 +4,22 @@
 
 namespace engine {
 
+// Integer types
 typedef uint8_t uint8;
 typedef uint16_t uint16;
 typedef uint32_t uint32;
 typedef uint64_t uint64;
-
 typedef int8_t int8;
 typedef int16_t int16;
 typedef int32_t int32;
 typedef int64_t int64;
 
+// Byte type
 typedef uint8 byte;
+
+// Char types
+typedef char char8;
+typedef int16 char16;
+typedef int32 char32;
 
 }  // namespace engine
