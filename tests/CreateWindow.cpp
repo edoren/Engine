855ee6f5b08ff6f4381f43a5decf299358a1699c
Manuel Sabogal <mfer32@gmail.com>
2017-03-06 00:24:21 -0500

    Added CreateWindow test

---
+++ tests/CreateWindow.cpp
@@ -0,0 +1,57 @@
+#include <Core/Main.hpp>
+#include <Input/InputManager.hpp>
+#include <Renderer/Renderer.hpp>
+#include <System/StringFormat.hpp>
+
+#include <iostream>
+
+#ifdef ENGINE_DEBUG
+#define VULKAN_PLUGIN_NAME "vulkan-plugin-d"
+#define OPENGL_PLUGIN_NAME "opengl-plugin-d"
+#else
+#define VULKAN_PLUGIN_NAME "vulkan-plugin"
+#define OPENGL_PLUGIN_NAME "opengl-plugin"
+#endif
+
+using namespace engine;
+
+int main(int argc, char* argv[]) {
+    Main engine(argc, argv);
+
+    String plugin = (argc == 2) ? argv[1] : "";
+    if (plugin == "vulkan") {
+        engine.LoadPlugin(VULKAN_PLUGIN_NAME);
+    } else if (plugin == "opengl") {
+        engine.LoadPlugin(OPENGL_PLUGIN_NAME);
+    } else {
+        String usage_msg = "Usage: {} [vulkan|opengl]"_format(argv[0]);
+        std::cout << usage_msg.ToUtf8() << std::endl;
+        return 0;
+    }
+
+    engine.Initialize();
+
+    InputManager& input = InputManager::GetInstance();
+    Renderer& render = engine.GetActiveRenderer();
+
+    math::ivec2 window_size = {800, 600};
+    RenderWindow& window = render.GetRenderWindow();
+
+    bool ok = window.Create("My Game Test", window_size);
+
+    if (!ok) {
+        LogError("main", "Init failed, exiting!");
+        return 1;
+    }
+
+    while (!input.exit_requested()) {
+        window.Clear(Color::BLACK);
+
+        input.AdvanceFrame(&window_size);
+        render.AdvanceFrame();
+    }
+
+    engine.Shutdown();
+
+    return 0;
+}
