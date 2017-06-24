9cfe08878d1e9b0c48c4b96ad1fea9e71240bdc1
Manuel Sabogal <mfer32@gmail.com>
2017-06-24 18:58:04 -0500

    Added additional Android macro to Platform.hpp

--- src/Util/Platform.hpp
+++ src/Util/Platform.hpp
@@ -20,41 +20,41 @@
 
 // Find the current platform and platform type
 #if defined(_WIN32)
-#define PLATFORM PLATFORM_WINDOWS
-#define PLATFORM_TYPE PLATFORM_TYPE_DESKTOP
+    #define PLATFORM PLATFORM_WINDOWS
+    #define PLATFORM_TYPE PLATFORM_TYPE_DESKTOP
 #elif defined(__linux__)
-#if !defined(__ANDROID__)
-#define PLATFORM PLATFORM_LINUX
-#define PLATFORM_TYPE PLATFORM_TYPE_DESKTOP
-#else
-#define PLATFORM PLATFORM_ANDROID
-#define PLATFORM_TYPE PLATFORM_TYPE_MOBILE
-#endif
+    #if defined(__ANDROID__) || defined(ANDROID)
+        #define PLATFORM PLATFORM_ANDROID
+        #define PLATFORM_TYPE PLATFORM_TYPE_MOBILE
+    #else
+        #define PLATFORM PLATFORM_LINUX
+        #define PLATFORM_TYPE PLATFORM_TYPE_DESKTOP
+    #endif
 #elif defined(__APPLE__) && defined(__MACH__)
-#if TARGET_IPHONE_SIMULATOR == 1 || TARGET_OS_IPHONE == 1
-#define PLATFORM PLATFORM_IOS
-#define PLATFORM_TYPE PLATFORM_TYPE_MOBILE
-#elif TARGET_OS_MAC == 1
-#define PLATFORM PLATFORM_MAC
-#define PLATFORM_TYPE PLATFORM_TYPE_DESKTOP
-#endif
+    #if TARGET_IPHONE_SIMULATOR == 1 || TARGET_OS_IPHONE == 1
+        #define PLATFORM PLATFORM_IOS
+        #define PLATFORM_TYPE PLATFORM_TYPE_MOBILE
+    #elif TARGET_OS_MAC == 1
+        #define PLATFORM PLATFORM_MAC
+        #define PLATFORM_TYPE PLATFORM_TYPE_DESKTOP
+    #endif
 #else
-#define PLATFORM PLATFORM_UNKNOWN
-#define PLATFORM_TYPE PLATFORM_TYPE_UNKNOWN
+    #define PLATFORM PLATFORM_UNKNOWN
+    #define PLATFORM_TYPE PLATFORM_TYPE_UNKNOWN
 #endif
 
 // Detect the platform avaliable APIs
 #if defined(_WIN32)
-#if defined(_WIN64)
-#define PLATFORM_APIS PLATFORM_API_WIN32 & PLATFORM_API_WIN64
-#else
-#define PLATFORM_APIS PLATFORM_API_WIN32
-#endif
+    #if defined(_WIN64)
+        #define PLATFORM_APIS PLATFORM_API_WIN32 & PLATFORM_API_WIN64
+    #else
+        #define PLATFORM_APIS PLATFORM_API_WIN32
+    #endif
 #elif defined(__unix__) || (defined(__APPLE__) && defined(__MACH__)) /* UNIX-style OS. */
-#if defined(_POSIX_VERSION) /* POSIX compliant */
-#undef PLATFORM_APIS
-#define PLATFORM_APIS PLATFORM_API_POSIX
-#endif
+    #if defined(_POSIX_VERSION) /* POSIX compliant */
+        #undef PLATFORM_APIS
+        #define PLATFORM_APIS PLATFORM_API_POSIX
+    #endif
 #endif
 
 #define PLATFORM_IS(x) PLATFORM == x
