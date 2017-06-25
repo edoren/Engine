0fb672be969726d7449dc845263ebc77d481f661
Manuel Sabogal <mfer32@gmail.com>
2017-06-24 19:10:58 -0500

    Indent some macros to improve readability

--- src/Renderer/Vulkan/Vk_Dependencies.hpp
+++ src/Renderer/Vulkan/Vk_Dependencies.hpp
@@ -3,15 +3,15 @@
 #include <Util/Platform.hpp>
 
 #if PLATFORM_IS(PLATFORM_WINDOWS)
-#define VK_USE_PLATFORM_WIN32_KHR
-#include <windows.h>
+    #define VK_USE_PLATFORM_WIN32_KHR
+    #include <windows.h>
 #elif PLATFORM_IS(PLATFORM_LINUX)
-#define VK_USE_PLATFORM_XCB_KHR
-#include <X11/Xlib-xcb.h>
-// #define VK_USE_PLATFORM_WAYLAND_KHR
-// #define VK_USE_PLATFORM_MIR_KHR
+    #define VK_USE_PLATFORM_XCB_KHR
+    #include <X11/Xlib-xcb.h>
+    // #define VK_USE_PLATFORM_WAYLAND_KHR
+    // #define VK_USE_PLATFORM_MIR_KHR
 #elif PLATFORM_IS(PLATFORM_ANDROID)
-#define VK_USE_PLATFORM_ANDROID_KHR
+    #define VK_USE_PLATFORM_ANDROID_KHR
 #endif
 
 #define VULKAN_HPP_NO_SMART_HANDLE
@@ -23,6 +23,6 @@
 
 // On Windows undefine this anoying macros defined by windows.h
 #if PLATFORM_IS(PLATFORM_WINDOWS)
-#undef ERROR
-#undef TRANSPARENT
+    #undef ERROR
+    #undef TRANSPARENT
 #endif
