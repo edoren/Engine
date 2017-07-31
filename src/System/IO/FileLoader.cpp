d10bee9da058cffc018d1a7d614cc35f6edcece5
Manuel Sabogal <mfer32@gmail.com>
2017-07-30 23:36:53 -0500

    Added alternative lookup paths to FileLoader

--- src/System/IO/FileLoader.cpp
+++ src/System/IO/FileLoader.cpp
@@ -7,6 +7,14 @@ namespace engine {
 
 namespace io {
 
+namespace {
+#if PLATFORM_IS(PLATFORM_ANDROID)
+    std::vector<String> sLookupPaths = {""};
+#else
+    std::vector<String> sLookupPaths = {"data/"};
+#endif
+}
+
 bool FileLoader::LoadFile(const String& filename, String* dest) {
     std::vector<byte> out;
     bool success = LoadFile(filename, &out);
@@ -18,11 +26,17 @@ bool FileLoader::LoadFile(const String& filename, String* dest) {
 }
 
 bool FileLoader::LoadFile(const String& filename, std::vector<byte>* dest) {
-    SDL_RWops* handle = SDL_RWFromFile(filename.GetData(), "rb");
+    SDL_RWops* handle = nullptr;
+    for (const String& path : sLookupPaths) {
+        String file_path = path + filename;
+        handle = SDL_RWFromFile(file_path.GetData(), "rb");
+        if (handle) break;
+    }
     if (!handle) {
         LogError("FileLoader", "LoadFile fail on " + filename);
         return false;
     }
+
     std::size_t len =
         static_cast<std::size_t>(SDL_RWseek(handle, 0, RW_SEEK_END));
     SDL_RWseek(handle, 0, RW_SEEK_SET);
