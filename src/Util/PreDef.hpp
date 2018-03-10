ca6ed932c29413e39a4834a122fb57b6c2526691
Manuel Sabogal <mfer32@gmail.com>
2018-03-10 10:58:30 -0500

    Remove exceptions in Release builds

--- src/Util/PreDef.hpp
+++ src/Util/PreDef.hpp
@@ -1,3 +1,17 @@
 #pragma once
 
 #define ENGINE_UNUSED(var) (void)var
+
+#ifndef ENGINE_DEBUG
+    #define ENGINE_NOEXCEPTION
+#endif
+
+#if (defined(__cpp_exceptions) || defined(__EXCEPTIONS) || defined(_CPPUNWIND)) && !defined(JSON_NOEXCEPTION)
+    #define ENGINE_THROW(exception) throw exception
+    #define ENGINE_TRY try
+    #define ENGINE_CATCH(exception) catch(exception)
+#else
+    #define ENGINE_THROW(exception) std::abort()
+    #define ENGINE_TRY if(true)
+    #define ENGINE_CATCH(exception) if(false)
+#endif
